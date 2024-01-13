750b825e4b8922055cc60e7871f38c7f62eecb52
Autumn60 <r117159@g.kobe-kosen.ac.jp>
2024-01-13 19:19:57 +0900

    Add doxygen comment to Visualizer.cs

--- Assets/UnitySensors/Runtime/Scripts/Visualizers/Visualizer.cs
+++ Assets/UnitySensors/Runtime/Scripts/Visualizers/Visualizer.cs
@@ -3,6 +3,10 @@ using UnitySensors.Sensor;
 
 namespace UnitySensors.Visualization
 {
+    /// <summary>
+    /// Base class for sensor data visualization class.
+    /// </summary>
+    /// <typeparam name="T"> Sensor for visualization target. </typeparam>
     public abstract class Visualizer<T> : MonoBehaviour where T : UnitySensor
     {
         private T _sensor;
@@ -16,7 +20,14 @@ namespace UnitySensors.Visualization
             Init();
         }
 
+        /// <summary>
+        /// Initialization function. Called at Start().
+        /// </summary>
         protected abstract void Init();
+
+        /// <summary>
+        /// Called when sensor is updated.
+        /// </summary>
         protected abstract void Visualize();
     }
 }
